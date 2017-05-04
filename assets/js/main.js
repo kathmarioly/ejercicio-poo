4b359274f00dc543cc71454437fc1fce3653feb9
kathmarioly <katerinemarioly@gmail.com>
2017-05-04 18:34:58 -0300

    Primer reporte

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,27 @@
-alert("gjgjtugjtu");
+
+function Pokemon(nombre, color, poderDeAtaque){
+	this.nombre = nombre;
+	this.color = color;
+
+	this.nivelDeAmistad = 0;
+
+	this.vida = 100;
+	this.poderDeAtaque = poderDeAtaque;
+
+	this.mostrarPokemon = function(){
+		return("Hola, soy: " + this.nombre + "y doy de color: " + this.color)
+	}
+
+	this.mostrarAmistad = function(){
+		this.nivelDeAmistad = this.nivelDeAmistad + valor
+	}
+	this.atacar = function(pokemon){
+		pokemon.vida = pokemon.vida - this.poderDeAtaque
+	}
+}
+
+const Pikachu = new Pokemon("Pikachu", "amarillo", 100)
+const Charmander = new Pokemon("Charmander", "rojo", 20)
+
+Pikachu.atacar(Charmander)
+document.write(Charmander.vida)
