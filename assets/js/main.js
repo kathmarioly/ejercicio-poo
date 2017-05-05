675607c17841adfc7ec45ba5ea347990a56548e9
kathmarioly <katerinemarioly@gmail.com>
2017-05-04 23:47:48 -0300

    Validación de botón

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,10 +27,24 @@ const Bulbasaur = new Pokemon("Bulbasaur" "celeste", 100)
 Pikachu.atacar(Charmander)
 document.write("La cantidad de vidas de tu Charmander es de: " + Charmander.vida +"<br>" + Pikachu.mostrarPokemon());
 
-function Boton(){
+function pelea(){
 	var boton = document.getElementById("boton").value;
 	var opcion1 = document.getElementById("opcion1").value;
-	var opcion1 = document.getElementById("opcion2").value;
+	var opcion2 = document.getElementById("opcion2").value;
+	var opcion3 = document.getElementById("opcion3").value;
+	var opcion4 = document.getElementById("opcion4").value;
+
+if (opcion3 == opcion4) {
+	alert("Los pokemon no pueden pelear contra si mismos")
+}else{
+
+var poder = prompt("Ingresa tu nivel de ataque");
+var combate1 = new Pokemon(opcion3, + color, + poder);
+var combate2 = new Pokemon(opcion4, + color, + poderDeAtaque);
+	combate1.atacar(combate2);
+}
+
+boton.innerHTML = "<br" + combate1.nombre + " atacó a" + combate2.nombre + " y " + combate2.nombre + " su vida es de: " + combate2.vida;
 
 }
 
