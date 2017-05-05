5c2b6c4a39d62cded2c2ee2a5d6f268ed6c67a5a
kathmarioly <katerinemarioly@gmail.com>
2017-05-04 22:22:57 -0300

    Agregando botón combate

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,6 +21,17 @@ function Pokemon(nombre, color, poderDeAtaque){
 
 const Pikachu = new Pokemon("Pikachu", "amarillo", 100)
 const Charmander = new Pokemon("Charmander", "rojo", 100)
+const Fennekin = new Pokemon("Fennekin", "amarillo con naranjo", 100)
+const Bulbasaur = new Pokemon("Bulbasaur" "celeste", 100)
 
 Pikachu.atacar(Charmander)
 document.write("La cantidad de vidas de tu Charmander es de: " + Charmander.vida +"<br>" + Pikachu.mostrarPokemon());
+
+function Boton(){
+	var boton = document.getElementById("boton").value;
+	var opcion1 = document.getElementById("opcion1").value;
+	var opcion1 = document.getElementById("opcion2").value;
+
+}
+
+
